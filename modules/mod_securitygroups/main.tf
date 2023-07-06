236a9cf68d4b7318508daa08687ce30c9ce72567
abasler43 <115531948+abasler43@users.noreply.github.com>
2023-07-06 07:19:38 +0200

    Create main.tf

---
+++ modules/mod_securitygroups/main.tf
@@ -0,0 +1,34 @@
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.51.1"
+    }
+  }
+}
+resource "openstack_compute_secgroup_v2" "http_group" {
+  name        = "http_secgroup"
+  description = "my security group to get HTTP server"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+    rule {
+    from_port   = 443
+    to_port     = 443
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
