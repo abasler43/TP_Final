7a4627d3135c1583f72c1c5a1f0031beb28acea2
abasler43 <115531948+abasler43@users.noreply.github.com>
2023-07-06 07:08:18 +0200

    Create vars.tf

---
+++ modules/mod_computes/vars.tf
@@ -0,0 +1,22 @@
+variable "compute_name" {
+  
+}
+
+variable "compute_image_id" {
+  
+}
+
+variable "compute_flavor_id" {
+  
+}
+
+variable "compute_key_pair" {
+  
+}
+
+variable "compute_secgroup" {
+}
+
+variable "compute_network_name" {
+  
+}
